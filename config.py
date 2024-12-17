a3ad292ea16e041c00855e9296af17c331d49efb
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-17 11:28:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -86,16 +86,16 @@ START_IMG_URL = getenv(
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://envs.sh/CLg.jpeg"
 )
-PLAYLIST_IMG_URL = "https://graph.org//file/3dfcffd0c218ead96b102.png"
-STATS_IMG_URL = "https://graph.org//file/99a8a9c13bb01f9ac7d98.png"
-TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-TELEGRAM_VIDEO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://envs.sh/CwU.jpg"
+STATS_IMG_URL = "https://envs.sh/CwU.jpg"
+TELEGRAM_AUDIO_URL = "https://envs.sh/CwU.jpg"
+TELEGRAM_VIDEO_URL = "https://envs.sh/CwU.jpg"
+STREAM_IMG_URL = "https://envs.sh/CwU.jpg"
+SOUNCLOUD_IMG_URL = "https://envs.sh/CwU.jpg"
+YOUTUBE_IMG_URL = "https://envs.sh/CwU.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://envs.sh/CwU.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://envs.sh/CwU.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://envs.sh/CwU.jpg"
 
 
 def time_to_seconds(time):
