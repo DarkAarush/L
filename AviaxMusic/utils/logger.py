f0633b2cffa01aabe78a60abd0e6674a4ee6ee7f
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-17 10:09:09 +0530

    Update logger.py

--- AviaxMusic/utils/logger.py
+++ AviaxMusic/utils/logger.py
@@ -8,18 +8,18 @@ from config import LOG_GROUP_ID
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+<blockquote><b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
+<b>𝗖𝗵𝗮𝘁 𝗶𝗱 :</b> <code>{message.chat.id}</code>
+<b>𝗖𝗵𝗮𝘁 𝗡𝗮𝗺𝗲 :</b> {message.chat.title}
+<b>𝗖𝗵𝗮𝘁 𝗨𝘀𝗲𝗿𝗻𝗮𝗺𝗲 :</b> @{message.chat.username}
 
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+<b>𝗨𝘀𝗲𝗿 𝗜𝗱 :</b> <code>{message.from_user.id}</code>
+<b>𝗣𝗿𝗼𝗳𝗶𝗹𝗲 𝗡𝗮𝗺𝗲:</b> {message.from_user.mention}
+<b>𝗨𝘀𝗲𝗿𝗻𝗮𝗺𝗲 :</b> @{message.from_user.username}
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+<b>𝗦𝗼𝗻𝗴 :</b> {message.text.split(None, 1)[1]}
+<b>𝗥𝗲𝗾𝘂𝗲𝘀𝘁 𝗣𝗹𝗮𝘁𝗳𝗼𝗿𝗺:</b> {streamtype}</blockquote>"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
